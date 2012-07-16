42df3b6984de5859c86b5b69cb3c009a61e72c37
Elsa Gonsiorowski (Uranus) <me@gonsie.com>
2012-07-16 15:43:12 -0400

    added full_parser file

---
+++ full_parser.py
@@ -0,0 +1,28 @@
+# Encapsulation of lexer/parser pair
+class PLYPair:
+	def __init__(self, l=None, p=None):
+		self.lexer = l
+		self.parser = p
+
+	def set_lexer(self, l):
+		self.lexer = l
+
+	def set_parser(self, p):
+		self.parser = p
+
+	def parse(self, fname):
+		f = open(fname, 'r')
+		a = f.read()
+		f.close()
+
+		return self.parser.parse(a, lexer=self.lexer)
+
+from Netlist import verilog_netlist
+
+if __name__ == "__main__":
+		vn = PLYPair()
+		vn.set_lexer(verilog_netlist.create_lexer({'and':'CELL','or':'CELL'}))
+		vn.set_parser(verilog_netlist.create_parser())
+		vn.parse('example_netlist.v')
+
+
