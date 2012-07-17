fa7178ca7abb8380b7c9e30eec1c9decafbac7df
Elsa Gonsiorowski (Uranus) <me@gonsie.com>
2012-07-17 17:58:13 -0400

    added an lsi_10k parsing example

--- full_parser.py
+++ full_parser.py
@@ -26,35 +26,51 @@ import ply_liberty
 import ply_boolean_expressions
 
 if __name__ == "__main__":
-	print "\n*** Liberty Parser"
-	l = PLYPair()
-	l.set_lexer(ply_liberty.create_lexer())
-	l.set_parser(ply_liberty.create_parser())
-	l.parse_file('Examples/example_library.lib')
-	cd = l.result.cell_tokens()
-	print l.result.stats
 
-	print "\n*** Verilog Netlist Parser"
-	vn = PLYPair()
-	vn.set_lexer(ply_verilog_netlist.create_lexer(cd))
-	vn.set_parser(ply_verilog_netlist.create_parser())
-	vn.parse_file('Examples/example_netlist.v')
+	if False:
+		print "\n*** Liberty Parser"
+		l = PLYPair()
+		l.set_lexer(ply_liberty.create_lexer())
+		l.set_parser(ply_liberty.create_parser())
+		l.parse_file('Examples/example_library.lib')
+		cd = l.result.cell_tokens()
+		print l.result.stats
 
-	print "\n*** Boolean Expression Parser"
-	be = PLYPair()
-	be.set_lexer(ply_boolean_expressions.create_lexer())
-	be.set_parser(ply_boolean_expressions.create_parser())
+		print "\n*** Verilog Netlist Parser"
+		vn = PLYPair()
+		vn.set_lexer(ply_verilog_netlist.create_lexer(cd))
+		vn.set_parser(ply_verilog_netlist.create_parser())
+		vn.parse_file('Examples/example_netlist.v')
 
-	for cell_name in cd:
-		cell = l.result.get_cell(cell_name)
-		pd = cell.pin_tokens()
-		pm = cell.pin_map()
+		print "\n*** Boolean Expression Parser"
+		be = PLYPair()
+		be.set_lexer(ply_boolean_expressions.create_lexer())
+		be.set_parser(ply_boolean_expressions.create_parser())
 
-		# this only works because of introspection at runtime
-		ply_boolean_expressions.update(pd, pm)
+		for cell_name in cd:
+			cell = l.result.get_cell(cell_name)
+			pd = cell.pin_tokens()
+			pm = cell.pin_map()
 
-		print cell_name
-		for p in cell.boolexps():
-			print "\tPin", p.name, "=", p.function, "\t--> ", p.cstr, "=", be.parse(p.function)
+			# this only works because of introspection at runtime
+			ply_boolean_expressions.update(pd, pm)
+
+			print cell_name
+			for p in cell.boolexps():
+				print "\tPin", p.name, "=", p.function, "\t--> ", p.cstr, "=", be.parse(p.function)
+				
+	# lsi_10k example
+	lsi_lib = PLYPair()
+	lsi_lib.set_lexer(ply_liberty.create_lexer())
+	lsi_lib.set_parser(ply_liberty.create_parser())
+	lsi_lib.parse_file('Examples/lsi_10k.lib')
+	cd = lsi_lib.result.cell_tokens()
+	print lsi_lib.result.stats
+
+	ccx = PLYPair()
+	ccx.set_lexer(ply_verilog_netlist.create_lexer(cd))
+	ccx.set_parser(ply_verilog_netlist.create_parser())
+	ccx.parse_file('Examples/ccx_lsi.vSyn')
+
 
 
