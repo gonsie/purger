4ed3d8331be884929acd91d554ab5206359c332f
Elsa Gonsiorowski (Uranus) <me@gonsie.com>
2015-10-26 17:02:06 -0400

    removed extraneous rtl code

--- Data/mcu_addrdp_dp.v
+++ Data/mcu_addrdp_dp.v
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d8dc1a996a39e69a58f6ae775bb2885acec0397ae5c066c0f071503b5a2d5de4
-size 24996
+oid sha256:c861091c9082157e81f1ba38fa22959e333dd8481c14f7612c0650089cfa5e5b
+size 9796
