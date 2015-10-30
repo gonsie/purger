8c6f26f8fccb12e9fbb352b684898911dc18aed1
Elsa Gonsiorowski (Uranus) <me@gonsie.com>
2015-10-30 14:30:21 -0400

    recursion is the answer
    what was the question?
    RECURSION

--- instantiate.py
+++ instantiate.py
@@ -1,17 +1,45 @@
 
 import os.path
 
-def load_clist(fname):
+g_prefix = "Current"
+
+class Module:
+    def __init__(self, name, count):
+        self.name = name
+        self.count = count
+        self.parent = None
+        self.children = []
+        self.gate_count = 0
+
+    def load(self):
+        self.children = load_clist(self.name)
+        for c in self.children:
+            c.parent = self.name
+            c.load()
+        self.gate_count = load_md(self.name)
+
+    def printDepth(self, depth):
+        ostr = "\t"*depth
+        ostr += str(self.count) + " " + self.name
+        print ostr
+        for c in self.children:
+            c.printDepth(depth+1)
+
+def load_clist(mod_name):
+    fname = "Data/"+mod_name+".clist"
+    if not os.path.isfile(fname):
+        return []
     r = []
     f = open(fname, 'r')
     for line in f:
         line = line.strip().split(' ')
         line[0] = int(line[0])
-        r.append(tuple(line))
+        r.append(Module(line[1], line[0]))
     f.close()
     return r
 
-def load_md(fname):
+def load_md(mod_name):
+    fname = "Generated/"+mod_name+"/"+mod_name+".metadata"
     f = open(fname, 'r')
     line = f.readline().strip()
     f.close()
@@ -21,7 +49,7 @@ def calc_offsets(top_level):
     offset_list = []
     offset = 0
     r = [(1, top_level)]
-    for i, n in r: 
+    for i, n in r:
         #i, n = r.pop(0)
         d = "Data/"+n+".clist"
         if os.path.isfile(d):
@@ -50,12 +78,18 @@ def calc_routing(offset_list):
 def main(top_level):
     ol = calc_offsets(top_level)
     rt = calc_routing(ol)
-    for l in rt:
-        print l
+
+def process(top_level):
+    top = Module(top_level, 1)
+    top.load()
+    top.printDepth(0)
 
 import sys
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print "Usage:", sys.argv[0], "top_level_name"
+        print "Usage:", sys.argv[0], "top_level_name [prefix]"
     else:
+        if len(sys.argv) == 3:
+            global g_prefix
+            g_prefix = sys.argv[2]
         main(sys.argv[1])
