49eb254c46d22a027d79aea09b32df2858596550
Elsa Gonsiorowski (Uranus) <me@gonsie.com>
2015-11-05 12:18:49 -0500

    fanout gates need internal array

--- file_writer.py
+++ file_writer.py
@@ -46,6 +46,9 @@ def write_gate_c(filename_prefix, types_list, gate_types):
 	# internal size
 	f.write("\nint gate_internal_size[GATE_TYPE_COUNT] = {\n\t")
 	for t in types_list:
+		if t == "fanout_gate":
+			f.write("2, ")
+		else:
 		f.write(str(gate_types[t].counts['internal']) + ", ")
 	f.write("\n\t};\n")
 	# output size
